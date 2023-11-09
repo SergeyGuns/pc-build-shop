0aa0f79c4e7c823139843f1a298be48521dd8d04
Ganin Sergey <mcheguevara2@gmail.com>
2023-11-09 09:30:58 +0300

    7160c6b

---
+++ index.2cb918e6.js
@@ -0,0 +1,3 @@
+//# sourceMappingURL=index.2cb918e6.js.map
+
+//# sourceMappingURL=index.2cb918e6.js.map
---
+++ perfomance.e7720543.js
@@ -0,0 +1,5 @@
+(()=>{
+    fetch("./perfomance_result.json").then((data)=>data.json().then(console.log));
+})();
+
+//# sourceMappingURL=perfomance.e7720543.js.map
